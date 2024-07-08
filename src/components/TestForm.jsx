20123a6e5be591b0e2cb2b712f0c0fc92cfd89fb
Vijendra Rai <vijendra.r@cisinlabs.com>
2024-07-08 18:03:33 +0530

    add day hours calculator

--- src/components/TestForm.jsx
+++ src/components/TestForm.jsx
@@ -3,24 +3,25 @@ import { useState } from 'react'
 
 export default function TestForm(props) {
   const [text, setText] = useState('');
-  const [result, setResult] = useState({ calculateHours: 0, getWorkingHours: { hours: 0, minutes: 0 } });
+  const [result, setResult] = useState({ calculateHours: 0, getWorkingHours: { hours: 0, minutes: 0 }, considerWorkingHours: null });
 
   useEffect(() => {
     const data = text.extractTimes()
     const calculateHours = data
       .reduce((acc, item) => acc + getTime(item), 0)
       .calculateHours(data.length);
-
+    console.log("calculateHours", calculateHours, data)
     const getWorkingHours = calculateHours.getWorkingHours();
     if (calculateHours !== 0) {
-      document.body.style.backgroundColor = calculateHours < 0 ? '#650808' : '#063806';
+      // document.body.style.backgroundColor = calculateHours < 0 ? '#650808' : '#063806';
       setResult({
         calculateHours,
-        getWorkingHours
+        getWorkingHours,
+        considerWorkingHours: data && data.length > 0 ? data : null
       });
     } else {
       setResult({ calculateHours: 0, getWorkingHours: { hours: 0, minutes: 0 } });
-      document.body.style.backgroundColor = '#ffffff';
+      // document.body.style.backgroundColor = '#ffffff';
     }
   }, [text])
 
@@ -31,11 +32,38 @@ export default function TestForm(props) {
       <div className="mb-3">
         <textarea className="form-control" style={{ backgroundColor: props.mode === 'light' ? 'white' : '#212529', color: props.mode === 'light' ? 'black' : 'white' }} onChange={e => setText(e.target.value)} value={text} id="mybox1" rows="8"></textarea>
       </div>
-      { result?.calculateHours !== 0 && <>
-        <img src={ result?.calculateHours < 0 ? './fail.gif': './pass.gif'} alt='Result'/>
+      {result?.calculateHours !== 0 && <>
+        <img src={result?.calculateHours < 0 ? './fail.gif' : './pass.gif'} alt='Result' />
         <h2 style={{ color: props.mode === 'light' ? '#777777' : '#ffcd39' }}>Hours Summary</h2>
-        <p style={{ color: props.mode === 'light' ? 'white' : 'white' }}>{`${result?.calculateHours < 0 ? 'Lagging Behind' : 'Ahead By'} => ${result?.getWorkingHours?.hours} Hours : ${result?.getWorkingHours?.minutes} minutes`}</p>
+        <p className={ `${ result?.calculateHours < 0 ? 'text-success': 'text-danger' }` }>{`${result?.calculateHours < 0 ? 'Lagging Behind' : 'Ahead By'} => ${result?.getWorkingHours?.hours} Hours : ${result?.getWorkingHours?.minutes} minutes`}</p>
       </>
+      }
+      {
+        result &&
+        result?.considerWorkingHours &&
+        <div className="table-responsive">
+          <table className={`table table-hover ${props.mode === 'light' ? '' : 'table-dark'}`}>
+            <thead>
+              <tr>
+                <th scope="col">Day</th>
+                <th scope="col">Hours</th>
+                <th scope="col">Required</th>
+                <th scope="col">Result</th>
+              </tr>
+            </thead>
+            <tbody>
+              {result.considerWorkingHours.map((workingHours, i) => (
+                <tr key={`working-hours-${i}`} className={ workingHours?.compareTimes()?.status === "ahead" ? "table-success": "table-danger" }>
+                  <th scope="row">{i + 1}</th>
+                  <td>{workingHours}</td>
+                  <td>8 hrs, 0 min</td>
+                  <td> { workingHours?.compareTimes()?.msg } </td>
+                </tr>
+              ))
+              }
+            </tbody>
+          </table>
+        </div>
       }
     </div>
   )
--- src/service/utility.js
+++ src/service/utility.js
@@ -1,7 +1,8 @@
-Number.prototype.getWorkingHours = function () {
+Number.prototype.getWorkingHours = function (toString) {
   const int = parseInt(this)
   const hours = Math.floor(Math.abs(int) / 60);
   const minutes = Math.floor(Math.abs(int) % 60);
+  if (toString) return `${hours} Hours, ${minutes} minutes`;
   return { hours, minutes }
 }
 Number.prototype.calculateHours = function (totalDays) {
@@ -13,6 +14,25 @@ String.prototype.extractTimes = function () {
   const regex = /(\d+ hrs, \d+ min)/g;
   const matches = this.match(regex);
   return matches || [];
+}
+
+function parseTimeString(timeStr) {
+  const [hours, minutes] = timeStr.split(',').map(part => parseInt(part.match(/\d+/)[0], 10));
+  return hours * 60 + minutes;
+}
+
+String.prototype.compareTimes = function () {
+  const referenceTime = '8 hrs, 0 min';
+  const referenceMinutes = parseTimeString(referenceTime);
+  const givenMinutes = parseTimeString(this);
+
+  if (givenMinutes > referenceMinutes) {
+    return { msg: `Ahead by ${(givenMinutes - referenceMinutes).getWorkingHours(true)}.`, status: 'ahead' };
+  } else if (givenMinutes < referenceMinutes) {
+    return { msg: `Behind by ${(referenceMinutes - givenMinutes).getWorkingHours(true)}.`, status: 'behind' };
+  } else {
+    return 'No Ahead/Behind';
+  }
 }
 
 export default {}
